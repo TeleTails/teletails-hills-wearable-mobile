ac20655370b721ecd0308a0e5e1843257a03778c
Aleksandra Maria Czajka <aleksczajka@gmail.com>
2024-05-09 12:25:20 -0400

    latest with android

--- src/screens/wearable-connect-screen.js
+++ src/screens/wearable-connect-screen.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import { StyleSheet, View, NativeModules,
-  NativeEventEmitter, TouchableOpacity } from 'react-native';
-import { Screen, Line, Text, Icon, Input, Button } from '../components';
+  NativeEventEmitter, TouchableOpacity, PermissionsAndroid, Platform } from 'react-native';
+import { Screen, Line, Text, Icon, Input, Colors } from '../components';
 import { setItem, getItem } from '../../storage';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 import { ScrollView } from "react-native-gesture-handler";
 import BleManager from 'react-native-ble-manager';
+import { Picker }      from '@react-native-picker/picker';
 import { stringToBytes, bytesToString } from "convert-string";
+import { PetsController } from "../controllers";
+import { StringUtils }       from '../utils';
+
 
 var Buffer = require("buffer/").Buffer;
 
@@ -19,6 +23,7 @@ class WearableConnectScreen extends Component {
     super(props);
 
     this.state = {
+      screen: 0,
       peripherals: []
     }
 
@@ -34,9 +39,20 @@ class WearableConnectScreen extends Component {
     this.selectWifi = this.selectWifi.bind(this);
     this.updatePassword = this.updatePassword.bind(this);
     this.forceSync = this.forceSync.bind(this);
+
+    this.render_pet_selection = this.render_pet_selection.bind(this);
+    this.pet_selection_action = this.pet_selection_action.bind(this);
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+
+    let pets_response = await PetsController.getPets();
+    let is_success    = pets_response && pets_response.success;
+    let pets          = is_success && pets_response.data && pets_response.data.pets ? pets_response.data.pets : [];
+    this.setState({ pets: pets });
+
+    console.log('Platform', Platform)
+
     if (Platform.OS === 'android'){
       BleManager.enableBluetooth().then(() => {
         console.log('Bluetooth is turned on!');
@@ -72,7 +88,23 @@ class WearableConnectScreen extends Component {
         console.log('scan stopped');
       },
     );
-    if (Platform.OS === 'android' && Platform.Version >= 23) {
+
+    if (Platform.OS === 'android' && Platform.Version >= 31) {
+      PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      ]).then(result => {
+        if (result) {
+          console.debug(
+            '[handleAndroidPermissions] User accepts runtime permissions android 12+',
+          );
+        } else {
+          console.error(
+            '[handleAndroidPermissions] User refuses runtime permissions android 12+',
+          );
+        }
+      });
+    } else if (Platform.OS === 'android' && Platform.Version >= 23) {
       PermissionsAndroid.check(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
       ).then(result => {
@@ -129,6 +161,9 @@ class WearableConnectScreen extends Component {
           resolve(peripheralInfo)
         }, 1500)}).catch(err=>{
           console.log('err', err);
+          this.setState({
+            connection_error: true
+          })
         });
     })
   }
@@ -168,7 +203,9 @@ class WearableConnectScreen extends Component {
               resolve(true)
             })
           }).catch(err=>{
-            console.log('err', err)
+            this.setState({
+              connection_error: true
+            })
           })
         }
       })
@@ -200,6 +237,8 @@ class WearableConnectScreen extends Component {
   connectPeripheral(connected_peripheral) {
     this.setState({
       connected_peripheral,
+      screen: 3,
+      connection_error: false,
       retrievingWifi: true
     }, async ()=>{
       let peripheral_id = connected_peripheral.id;
@@ -264,6 +303,7 @@ class WearableConnectScreen extends Component {
     let peripheral_id = connected_peripheral.id;
     console.log('wifi_name', wifi_name)
     console.log('password', password)
+    password = password ? password : ""
 
     const WIFI_SSID_CHAR = 'A172F7E1-A5B8-11E5-A837-0800200C9A66';
     const WIFI_PSD_CHAR = 'A172F7E2-A5B8-11E5-A837-0800200C9A66';
@@ -294,6 +334,13 @@ class WearableConnectScreen extends Component {
     const url = stringToBytes(`tst.wearablesclinicaltrials.com`);
     await this.write(peripheral_id, "A172D0D1-A5B8-11E5-A837-0800200C9A66","A172F7E0-A5B8-11E5-A837-0800200C9A66",url);
 
+
+    let interval = setTimeout(()=>{
+      this.setState({
+        connection_error: true
+      })
+    }, 10)
+
     let writeVal = [1];
     // sync
     console.log('----------- write sync ---------')
@@ -306,6 +353,8 @@ class WearableConnectScreen extends Component {
    const eventLogType = buffer.readUInt8(0, true);
 
    console.log('eventLogType', eventLogType);
+
+    clearTimeout(interval)
 
    this.setState({
     eventLogType,
@@ -326,8 +375,41 @@ class WearableConnectScreen extends Component {
     })
   }
 
+  pet_selection_action(pet) {
+    this.setState({
+      selected_pet: pet,
+      screen: 1
+    })
+  }
+
+  render_pet_selection() {
+    let pets = this.state.pets || [];
+
+    let pet_rows = pets.map((pet) => {
+      let pet_name = StringUtils.displayName(pet);
+      return <View key={pet._id}>
+        <TouchableOpacity style={styles.selection_row_container} onPress={ () => { this.pet_selection_action(pet) }}>
+          <Text style={styles.selection_row_title}>{ pet_name }</Text>
+        </TouchableOpacity>
+        <Line />
+      </View>
+    })
+
+    return <View style={styles.section_container}>
+      <Text style={styles.section_title}>Select a pet</Text>
+      { pet_rows }
+      <TouchableOpacity onPress={ () => { this.props.navigation.push('PetDetailsEdit', { type: 'bio', add_new: true, success_action: () => { this.pull_pets() }}) }} style={{ flexDirection: 'row', alignItems: 'center', marginTop: 20 }}>
+        <Icon name='plus-circle' color={Colors.PRIMARY} />
+        <View style={{ marginLeft: 15 }}>
+          <Text style={{ fontSize: 15, fontWeight: 'medium', color: '#535353' }}>{ 'Add A New Pet' }</Text>
+          <Text style={{ fontSize: 14, color: '#575762', marginTop: 3 }}>{ 'Add another pet to your account' }</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  }
+
   render() {
-    let { isScanning, connected_peripheral, wifi_list, wifi_name, retrievingWifi, eventLogType, syncing } = this.state;
+    let { screen, isScanning, connected_peripheral, wifi_list, wifi_name, retrievingWifi, eventLogType, syncing, pet_setup, connection_error, password } = this.state;
 
     let peripherals = this.peripherals;
 
@@ -344,7 +426,7 @@ class WearableConnectScreen extends Component {
      */
 
     let codes = [
-      "0 means success code?",
+      "Sync Successful!",
       "No SSID, Wifi connection was not attempted",
       "Invalid Wi-Fi Network Name (SSID) or Password. Please check and try again.",
       "Invalid Wi-Fi Network Name (SSID). Please check and try again",
@@ -353,39 +435,77 @@ class WearableConnectScreen extends Component {
       "Failed Rudp server connection",
       "Wifi connection aborted due to low bandwidth between Sensor and Rudp server.",
       "Wifi connection aborted due to Sensor low battery condition detected during Rudp upload."
-    ]
-    
+    ];
+
+    let device_types = [{ name: 'AGL2', value: 'Sensor###AGL2' }, { name: 'AGL3', value: 'Sensor###AGL3' }, { name: 'HPN1', value: 'Sensor###HPN1' }];
+
     return <Screen title='Connect Device' scroll={true} navigation={this.props.navigation}>
       <ScrollView style={{flex: 1}}>
+        {screen === 0 ? <View>
+          {this.render_pet_selection()}
+        </View> : null}
+
+        {screen === 1 ? <View>
+          {pet_setup ? <View></View> : null}
+          {!pet_setup ? <View>
+            <Text>Setup Pet's Device</Text>
+            <View>
+              <Input type={'text'} placeholder={'Device Number'} onChangeText={(deviceNumber)=>{this.setState({deviceNumber})}} />
+
+              <Picker
+              style={{ borderRadius: 10, backgroundColor: 'white', padding: 10, paddingTop: 15 }}
+              selectedValue={this.state.pet_breed}
+              onValueChange={ (deviceType) => {
+                this.setState({ deviceType })
+              }}>
+                {device_types.map(type=><Picker.Item key={type.value} label={type.name} value={type.value} />)}
+                
+            </Picker>
+
+
+
+              <TouchableOpacity style={{padding: 20, backgroundColor: 'blue', margin: 40}} onPress={()=>{this.setState({screen: 2, pet_setup: false})}}><Text style={{ color: 'white' }}>Continue</Text></TouchableOpacity>
+            </View>
+          </View> : null}
+        </View> : null }
+
+        {screen === 2 ? <View style={{flexDirection: 'column'}}>
+            {isScanning ? <Text>Scanning ...</Text> :
+            <TouchableOpacity style={{backgroundColor: 'blue', padding: 20, margin: 40}} onPress={this.startScan}><Text style={{color: 'white'}}>Scan Devices</Text></TouchableOpacity>}
+
+            {peripherals && peripherals.length ? <View style={{flexDirection: 'column'}}>
+              <Text>Bluetooth Devices</Text>
+              <Text style={{color: 'red'}}>If you don't see your device listed below, please shake the device and hit the 'Scan Devices' button again.</Text>
+              {peripherals.map(a=><TouchableOpacity onPress={()=>{this.connectPeripheral(a)}}><Text style={{backgroundColor: 'blue', color: 'white', padding: 20}}>{a.name}</Text></TouchableOpacity>)}
+            </View> : null}
+          </View> : null}
+
+
         <View>
-          {connected_peripheral ? 
+          {screen === 3 ? 
           <View>
+            {connection_error ? <View>
+              <Text>Connection error: Please shake the device and try connecting again</Text>
+              <TouchableOpacity onPress={()=>{this.connectPeripheral(connected_peripheral)}}><Text style={{color: 'white', backgroundColor: 'green', padding: 20}}>Re-connect</Text></TouchableOpacity>
+              </View> : <>
             <Text>Connected to: {connected_peripheral.name}</Text>
             <View style={{flexDirection: 'column'}}>
               <Text>Wifi list</Text>
               {wifi_list ? <View>
                   {wifi_list.map(a=><View><TouchableOpacity onPress={()=>{this.selectWifi(a)}}><Text style={{color: 'white', backgroundColor: 'green', padding: 20}}>{a}</Text></TouchableOpacity>
                   {wifi_name && wifi_name === a ? <View>
-                    <Input type={'password'} placeholder={'type password'} onChangeText={this.updatePassword} />
+                    <Input value={password} type={'password'} placeholder={'type password'} onChangeText={this.updatePassword} />
                     <TouchableOpacity onPress={this.forceSync}><Text style={{color: 'white', backgroundColor: 'black', padding: 20}}>Connect & sync</Text></TouchableOpacity>
                   </View> : null}
 
                   </View>)}
                 </View> : null}
-              {retrievingWifi ? <Text>Retrieving ...</Text> : null}
+              {retrievingWifi && !syncing ? <Text>Retrieving ...</Text> : null}
               {syncing ? <Text style={{color: 'red'}}>Syncing...</Text> : null}
               {eventLogType || eventLogType === 0 ? <Text style={{color: 'red'}}>Sync result: {codes[eventLogType]}</Text> : null}
-            </View>
+            </View></>}
           </View> :
-          <View style={{flexDirection: 'column'}}>
-            {isScanning ? <Text>Scanning ...</Text> :
-            <TouchableOpacity style={{backgroundColor: 'black', padding: 20}} onPress={this.startScan}><Text style={{color: 'white'}}>Scan Devices</Text></TouchableOpacity>}
-
-            {peripherals && peripherals.length ? <View style={{flexDirection: 'column'}}>
-              <Text>Bluetooth Devices</Text>
-              {peripherals.map(a=><TouchableOpacity onPress={()=>{this.connectPeripheral(a)}}><Text style={{backgroundColor: 'blue', color: 'white', padding: 20}}>{a.name}</Text></TouchableOpacity>)}
-            </View> : null}
-          </View>}
+          null}
         </View>
       </ScrollView>
     </Screen>
@@ -396,5 +516,80 @@ class WearableConnectScreen extends Component {
 export default WearableConnectScreen;
 
 const styles = StyleSheet.create({
-
+  section_container: {
+    paddingRight: 20,
+    paddingLeft: 20
+  },
+  selection_row_container: {
+    flex: 1,
+    padding: 20,
+    paddingLeft: 0
+  },
+  selection_row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    height: 30
+  },
+  selection_row_title: {
+    fontSize: 15,
+    fontWeight: 'medium'
+  },
+  section_title_og: {
+    fontSize: 22,
+    fontWeight: 'semibold'
+  },
+  section_title: {
+    fontSize: 18,
+    fontWeight: 'medium',
+    color: '#0054A4'
+  },
+  progress_bar_container: {
+    paddingRight: 20,
+    paddingLeft: 20,
+    marginTop: 20,
+    marginBottom: 20
+  },
+  progress_bar: {
+    height: 10,
+    borderRadius: 20,
+    backgroundColor: '#e7e7e7'
+  },
+  triage_question: {
+    fontSize: 15,
+    marginTop: 15,
+    marginBottom: 15
+  },
+  triage_buttons_container: {
+    flexDirection: 'row'
+  },
+  triage_button: {
+    flex: 1,
+    backgroundColor: '#e7e7e7',
+    padding: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12
+  },
+  triage_button_title: {
+    fontWeight: 'semibold',
+    fontSize: 15,
+    color: '#474747',
+  },
+  triage_button_selected: {
+    flex: 1,
+    padding: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 12,
+    backgroundColor: Colors.PRIMARY
+  },
+  triage_button_title_selected: {
+    fontWeight: 'semibold',
+    fontSize: 15,
+    color: 'white',
+  },
+  triage_button_buffer: {
+    width: 10
+  }
 });
