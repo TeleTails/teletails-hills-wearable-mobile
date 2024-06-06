50453dd20dda3b66b0fb7a598d4571005bef1f02
Aleksandra Maria Czajka <aleksczajka@gmail.com>
2024-06-06 13:36:05 -0400

    latest with preview env vars

--- src/controllers/utilities-controller.js
+++ src/controllers/utilities-controller.js
@@ -4,9 +4,11 @@ import io from 'socket.io-client';
 
 let sockets = {};
 
-/* let API_URL = "https://prod-elb-api.teletails.com/data";
-let CHAT_URL = "https://prod-elb-chat.teletails.com";
-let NOTIF_URL = "https://prod-elb-chat.teletails.com"; */
+//let API_URL = "https://prod-elb-api.teletails.com/data";
+/* let CHAT_URL = "https://prod-elb-chat.teletails.com";
+let NOTIF_URL = "https://prod-elb-chat.teletails.com";
+let API_URL = 'https://1266-71-172-17-224.ngrok-free.app/data';
+ */
 /* let API_URL = "http://localhost:3000/data";
 let CHAT_URL = "https://prod-elb-chat.teletails.com";
 let NOTIF_URL = "https://prod-elb-chat.teletails.com"; */
